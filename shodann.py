67334e8d14a766edb87ded66dad53f6d9c4d9799
parvizx3 <129318294+parvizx3@users.noreply.github.com>
2023-08-16 20:14:02 +0330

    Update shodann.py

--- shodann.py
+++ shodann.py
@@ -1,8 +1,8 @@
 import shodan, os
 
 # Initialize Shodan API client
-api_key = os.environ.get("TOKEN")
-api = shodan.Shodan(api_key)
+api_keys = str(os.environ.get("TOKEN")).split(" ")
+
 
 # Define the search query
 query = os.environ.get("QUERY")
@@ -16,18 +16,20 @@ def get_server_addresses(results):
 
 try:
     # Get the first 2 pages of search results
-    for page in range(1, 3):
-        # Perform the search
-        results = api.search(query, page=page)
-
-        # Get server addresses from the current page
-        server_addresses = get_server_addresses(results)
-        
-        # Print server addresses
-        print(f"Page {page} - Server Addresses:")
-        for address in server_addresses:
-            print(address)
-        print()
+    for api_key in api_keys:
+        api = shodan.Shodan(api_key)
+        for page in range(1, 3):
+            # Perform the search
+            results = api.search(query, page=page)
+    
+            # Get server addresses from the current page
+            server_addresses = get_server_addresses(results)
+            
+            # Print server addresses
+            print(f"Page {page} - Server Addresses:")
+            for address in server_addresses:
+                print(address)
+            print()
 
 except shodan.APIError as e:
     print('Error: {}'.format(e))
